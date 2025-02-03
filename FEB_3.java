5319a67209a838f894e9ae1071f2af2427006af0
csai22064 <csai22064@glbitm.ac.in>
2025-02-03 18:34:23 +0530

    Add files via upload

---
+++ FEB_3.java
@@ -0,0 +1,16 @@
+package JAN_27;
+
+public class FEB_3 {
+    public static void main(String[] args) {
+        try{
+            //your statement goes here
+            int data = 12/0;
+
+            //Exception occurs in try block and handled in catch block
+        }
+        catch(Exception e){
+            //you can catch and handle your exception here
+            System.out.println(e);
+        }
+    }
+}
